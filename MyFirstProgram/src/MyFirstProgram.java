3461e4051a2e2efb6685eeecc365ce01570149de
Amalamol Raju <33655795+araju55@users.noreply.github.com>
2018-02-07 20:58:59 +0000

    Add files via upload

---
+++ MyFirstProgram/src/MyFirstProgram.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 17011947
+ */
+public class MyFirstProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        int width, height, area;
+
+        int width = 20;
+        int height = 30;
+        
+        int area = width * height;
+
+        System.out.println("Area is: " + area);
+
+    }
+
+}
